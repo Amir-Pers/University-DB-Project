a3a7de15a99218514bf6dec56f7f6f8595796ed7
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-06-19 14:45:50 +0330

    add 2 tabel to query
    + Address
    + Advertisement
    + add 3 index for faster search

--- phase2_sql_scripts/SQLQuery1.sql
+++ phase2_sql_scripts/SQLQuery1.sql
@@ -55,7 +55,7 @@ CREATE TABLE [User] (
 );
 GO
 
-CREATE NONCLUSTERED INDEX IX_User_Phone ON [User] (phone);
+CREATE INDEX IX_User_Phone ON [User] (phone);
 GO
 
 
@@ -114,4 +114,59 @@ CREATE TABLE HeavyVehicle(
         ON DELETE CASCADE
 );
 GO
+
+
+CREATE TABLE Address (
+    address_id INT PRIMARY KEY IDENTITY(1,1),
+    city_id INT NOT NULL,
+    neighborhood NVARCHAR(200) NULL,
+    CONSTRAINT FK_Address_City FOREIGN KEY (city_id) 
+        REFERENCES City(city_id)
+        ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX IX_Address_CityID ON Address (city_id);
+GO
+
+
+CREATE TABLE Advertisement (
+    ad_id INT PRIMARY KEY IDENTITY(1,1),
+    vehicle_id INT NOT NULL,
+    userid INT NOT NULL,
+    address_id INT NOT NULL,
+    title NVARCHAR(255) NOT NULL,
+    sell_type NVARCHAR(60) NOT NULL,
+    price DECIMAL(18,0) NULL,
+    descriptions NVARCHAR(MAX) NULL,
+    published BIT DEFAULT 1,                
+    created_date DATETIME DEFAULT GETDATE(),
+    updated_date DATETIME NULL,
+    ad_type NVARCHAR(50) NULL,              
+    car_condition NVARCHAR(50) NULL,          
+    remittance_time NVARCHAR(50) NULL,        
+    km_age INT NULL,                          
+    body_status NVARCHAR(50) NULL,           
+    free_zone BIT DEFAULT 0,                  
+    active_status BIT DEFAULT 1,              
+    CONSTRAINT FK_Advertisement_Vehicle FOREIGN KEY (vehicle_id) 
+        REFERENCES Vehicle(vehicle_id)
+        ON DELETE CASCADE,
+    CONSTRAINT FK_Advertisement_User FOREIGN KEY (userid) 
+        REFERENCES [User](userid)
+        ON DELETE CASCADE,
+    CONSTRAINT FK_Advertisement_Address FOREIGN KEY (address_id) 
+        REFERENCES Address(address_id)
+        ON DELETE CASCADE,
+    CONSTRAINT CHK_Advertisement_SellType CHECK (
+    sell_type IN (N'نقدی', N'اقساطی', N'توافقی')
+    )
+);
+GO
+
+CREATE INDEX IX_Advertisement_UserID ON Advertisement (userid);
+CREATE INDEX IX_Advertisement_AddressID ON Advertisement (address_id);
+CREATE INDEX IX_Advertisement_CreatedDate ON Advertisement (created_date DESC);
+GO
+
 
