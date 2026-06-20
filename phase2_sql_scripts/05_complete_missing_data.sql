075bd07a681319e1b04f126e84cbab638ff49dce
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-06-20 17:57:49 +0330

    Add SP2

--- phase2_sql_scripts/05_complete_missing_data.sql
+++ phase2_sql_scripts/05_complete_missing_data.sql
@@ -11,7 +11,7 @@ GO
 
 INSERT INTO Car (vehicle_id, body_type, engine, cylinder_volume, 
 	enginepower, torque, accelerate) 
-	VALUES  (1003,  N'سدان', N'6 سیلندر 24 سوپاپ', 2500, 200, 270, 7.50);
+	VALUES  (202,  N'سدان', N'6 سیلندر 24 سوپاپ', 2500, 200, 270, 7.50);
 Go
 
 
@@ -27,7 +27,28 @@ INSERT INTO Address (city_id, neighborhood)
 GO
 
 
+UPDATE [User]
+SET username = N'u4032'
+WHERE username = N'u4023';
+GO
 
+
+INSERT INTO Advertisement (vehicle_id, userid, address_id, title, sell_type, price, 
+	descriptions, published, created_date, updated_date, ad_type, car_condition, remittance_time, 
+	km_age, body_status, free_zone, active_status) VALUES
+(1,9,1,N'سمند سفید ۱۴۰۰',N'نقدی',350000000,N'کارکرد ۴۰ هزار کیلومتر',1,
+	GETDATE(),GETDATE(),N'عادی',N'کارکرده',N'۱ روزه',40000,N'بدون رنگ و زنگ',0,1),
+(14,9,2,N'هیوندای Santa Fe 2022 مشکی',N'نقدی',2800000000,N'کارکرد ۱۵ هزار کیلومتر',1,
+	GETDATE(),GETDATE(),N'نردبانی',N'کارکرده',N'۳ روزه',15000,N'بدون رنگ و زنگ',0,1),
+(103,9,3,N'بنز E200 2020 مشکی',N'توافقی',NULL,N'کارکرد ۲۵ هزار کیلومتر',1,
+	GETDATE(),GETDATE(),N'نردبانی',N'کارکرده',N'۵ روزه',25000,N'بدون رنگ و زنگ',0,1),
+(8,11,9,N'دنا پلاس سفید صفر',N'نقدی',650000000,N'صفر کیلومتر فاکتور',1,
+	GETDATE(),GETDATE(),N'عادی',N'صفر',N'همین روز',0,N'بدون رنگ و زنگ',0,1),
+(110,11,10,N'BMW X5 2020 مشکی',N'اقساطی',NULL,N'کارکرد ۳۰ هزار کیلومتر',1,GETDATE(),
+	GETDATE(),N'نردبانی',N'کارکرده',N'۵ روزه',30000,N'بدون رنگ و زنگ',0,1),
+(15,11,11,N'هیوندای Elantra 2021 نقره‌ای',N'نقدی',1700000000,N'کارکرد ۲۰ هزار کیلومتر',1,
+GETDATE(),GETDATE(),N'عادی',N'کارکرده',N'۲ روزه',20000,N'بدون رنگ و زنگ',0,1);
+GO
 
 
 select * from [User]
@@ -40,7 +61,7 @@ select * from Brand
 where brand_id = 3
 
 select * from Advertisement
-where vehicle_id=42
+where userid=11
 
 select * from Vehicle
 where model_id = 42
--- phase2_sql_scripts/06_stored_procedures.sql
+++ phase2_sql_scripts/06_stored_procedures.sql
@@ -72,5 +72,70 @@ EXEC InsertAdvertisement
     @Fuel = N'بنزین', @BodyStatus = N'بدون رنگ',
     @Gearbox = N'دنده‌ای', @Function = 0,
     @Color = N'سفید', @Province = N'فارس';
+GO
+
+
+-- SP2
+DROP PROCEDURE IF EXISTS SP_ShowAdvertisements;
+GO
+
+CREATE PROCEDURE SP_ShowAdvertisements
+    @Username1 NVARCHAR(50),  
+    @Username2 NVARCHAR(50)    
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @userid1 INT, @userid2 INT;
+
+    SET @userid1 = (SELECT userid FROM [User] WHERE username = @Username1);
+    SET @userid2 = (SELECT userid FROM [User] WHERE username = @Username2);
+
+    IF @userid1 IS NULL OR @userid2 IS NULL
+        RETURN;
+
+    SELECT * FROM Advertisement WHERE userid IN (@userid1, @userid2);
+
+    DELETE FROM Advertisement WHERE userid = @userid2;
+
+    SELECT * FROM Advertisement WHERE userid IN (@userid1, @userid2);
+END
+GO
+
+-- test SP2
+EXEC SP_ShowAdvertisements @Username1 = 'u4021', @Username2 = 'u4032';
+GO
+
+-- Record insertion for @Username2 = 'u4032'
+INSERT INTO Advertisement (vehicle_id, userid, address_id, title, sell_type, price, 
+	descriptions, published, created_date, updated_date, ad_type, car_condition, remittance_time, 
+	km_age, body_status, free_zone, active_status) VALUES
+(8,11,9,N'دنا پلاس سفید صفر',N'نقدی',650000000,N'صفر کیلومتر فاکتور',1,
+	GETDATE(),GETDATE(),N'عادی',N'صفر',N'همین روز',0,N'بدون رنگ و زنگ',0,1),
+(110,11,10,N'BMW X5 2020 مشکی',N'اقساطی',NULL,N'کارکرد ۳۰ هزار کیلومتر',1,GETDATE(),
+	GETDATE(),N'نردبانی',N'کارکرده',N'۵ روزه',30000,N'بدون رنگ و زنگ',0,1),
+(15,11,11,N'هیوندای Elantra 2021 نقره‌ای',N'نقدی',1700000000,N'کارکرد ۲۰ هزار کیلومتر',1,
+    GETDATE(),GETDATE(),N'عادی',N'کارکرده',N'۲ روزه',20000,N'بدون رنگ و زنگ',0,1);
+GO
+
+
+select * from [User]
+where username='u4021';
+
+select * from Model
+where name = 'MVM 530'
+
+select * from Brand
+where brand_id = 3
+
+select * from Advertisement
+where userid=11
+
+select * from Vehicle
+where model_id = 42
+
+select * from Address as A
+	inner join city as c
+		on A.city_id = C.city_id
 
 
