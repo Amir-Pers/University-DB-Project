9ceabc7bd49d4b42012a9cfa89b7b8cbd9d34cc6
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-06-20 19:02:52 +0330

    Add SP3

--- phase2_sql_scripts/06_stored_procedures.sql
+++ phase2_sql_scripts/06_stored_procedures.sql
@@ -119,6 +119,52 @@ INSERT INTO Advertisement (vehicle_id, userid, address_id, title, sell_type, pri
 GO
 
 
+-- SP3
+DROP PROCEDURE IF EXISTS SP_GetCarsByProvince;
+GO
+
+CREATE PROCEDURE SP_GetCarsByProvince
+    @ProvinceName NVARCHAR(100),
+    @BodyStatus_A NVARCHAR(100),
+    @BodyStatus_B NVARCHAR(100)
+AS
+BEGIN
+    SET NOCOUNT ON;
+
+    DECLARE @Province_id INT;
+    SET @Province_id = (SELECT province_id FROM Province WHERE name = @ProvinceName);
+
+    SELECT 
+        b.name AS BrandName,
+        m.name AS ModelName,
+        a.km_age AS KmAge
+    FROM Advertisement AS a
+        INNER JOIN Vehicle AS v ON a.vehicle_id = v.vehicle_id
+        INNER JOIN Model AS m ON v.model_id = m.model_id
+        INNER JOIN Brand AS b ON m.brand_id = b.brand_id
+    WHERE a.address_id IN (
+        SELECT address_id FROM Address
+        WHERE city_id IN (
+            SELECT city_id FROM City
+            WHERE province_id = @Province_id
+        )
+    )
+    AND a.active_status = 1
+    AND a.published = 1
+    AND a.body_status IN (@BodyStatus_A, @BodyStatus_B)
+    ORDER BY a.km_age;
+END
+GO
+
+
+--  test SP3
+EXEC SP_GetCarsByProvince 
+    @ProvinceName = N'البرز',
+    @BodyStatus_A = N'بدون رنگ',
+    @BodyStatus_B = N'کاپوت رنگ';
+
+
+
 select * from [User]
 where username='u4021';
 
