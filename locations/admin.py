d17f2a67584cd5d51328f54c3c18b51b2c6e2858
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-07-01 13:26:54 +0330

    Add locations models with admin configuration

--- locations/admin.py
+++ locations/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from .models import Province, City, Address
 
-# Register your models here.
+class ProvinceAdmin(admin.ModelAdmin):
+    list_display = ['province_id', 'name']
+    search_fields = ['name']
+
+
+class CityAdmin(admin.ModelAdmin):
+    list_display = ['city_id', 'name', 'province']
+    search_fields = ['name']
+    list_filter = ['province']
+
+
+class AddressAdmin(admin.ModelAdmin):
+    list_display = ['address_id', 'city', 'neighborhood']
+    search_fields = ("city__name", "neighborhood")
+    list_filter = ("city__province",)
+
+
+admin.site.register(Province, ProvinceAdmin)
+admin.site.register(City, CityAdmin)
+admin.site.register(Address, AddressAdmin)
+
+
--- locations/models.py
+++ locations/models.py
@@ -1,3 +1,43 @@
 from django.db import models
 
-# Create your models here.
+class Province(models.Model):
+    province_id = models.AutoField(primary_key=True)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'Province'
+        ordering = ["name"]
+
+    def __str__(self):
+        return self.name
+
+
+class City(models.Model):
+    city_id = models.AutoField(primary_key=True)
+    province = models.ForeignKey('Province', on_delete=models.CASCADE, related_name="cities",)
+    name = models.CharField(max_length=100)
+
+    class Meta:
+        managed = False
+        db_table = 'City'
+        ordering = ["name"]
+
+
+    def __str__(self):
+        return self.name
+
+
+class Address(models.Model):
+    address_id = models.AutoField(primary_key=True)
+    city = models.ForeignKey('City', on_delete=models.CASCADE, related_name="addresses")
+    neighborhood = models.CharField(max_length=200, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = 'Address'
+        
+    def __str__(self):
+        if self.neighborhood:
+            return f"{self.city.name} - {self.neighborhood}"
+        return self.city.name
