d2617de6ee9aa073059af2965d91282cc7fa3fec
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-07-01 13:03:01 +0330

    add user to accounts/model

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
+from .models import User
 
-# Register your models here.
+
+class UserAdmin(admin.ModelAdmin):
+    list_display=['userid', 'username', 'phone', 'reg_status', "account_status"]
+    search_fields = ['username', 'phone', 'national_id']
+    list_filter = ['reg_status', 'account_status']
+
+
+
+admin.site.register(User, UserAdmin)
--- accounts/models.py
+++ accounts/models.py
@@ -1,3 +1,20 @@
 from django.db import models
 
-# Create your models here.
+
+class User(models.Model):
+    userid = models.AutoField(primary_key=True)
+    phone = models.CharField(max_length=20, unique=True)
+    reg_status = models.BooleanField(default=False)
+    account_status = models.BooleanField(default=True)
+    register_date = models.DateTimeField(blank=True, null=True)
+    national_id = models.CharField(max_length=10, blank=True, null=True)
+    username = models.CharField(max_length=50, unique=True, blank=True, null=True)
+
+    class Meta:
+        managed = False
+        db_table = "User"
+        verbose_name = "User"
+        verbose_name_plural = "Users"
+
+    def __str__(self):
+        return self.username or self.phone
