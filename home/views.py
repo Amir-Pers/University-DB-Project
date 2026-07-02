a29be15eb589f8337798686cae9ba13b08affbab
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-07-02 19:01:29 +0330

    Convert Listing Cards To Django Templates

--- home/views.py
+++ home/views.py
@@ -1,11 +1,17 @@
+from django.db.models import Prefetch
 from django.shortcuts import render
-from advertisements.models import Advertisement
+
+from advertisements.models import Advertisement, Image
+from vehicles.models import Car, Motorcycle
 
 
 def index(request):
 
     advertisements = (
-        Advertisement.objects.filter(active_status=True, published=True)
+        Advertisement.objects.filter(
+            active_status=True,
+            published=True,
+        )
         .select_related(
             "vehicle",
             "vehicle__model",
@@ -14,11 +20,32 @@ def index(request):
             "address",
             "address__city",
         )
+        .prefetch_related(
+            Prefetch(
+                "images",
+                queryset=Image.objects.order_by("image_id"),
+            )
+        )
         .order_by("-created_date")
     )
 
+    total_ads = advertisements.count()
+
+    car_count = Car.objects.filter(
+        vehicle__advertisements__published=True,
+        vehicle__advertisements__active_status=True,
+    ).count()
+
+    motorcycle_count = Motorcycle.objects.filter(
+        vehicle__advertisements__published=True,
+        vehicle__advertisements__active_status=True,
+    ).count()
+
     context = {
         "advertisements": advertisements,
+        "total_ads": total_ads,
+        "car_count": car_count,
+        "motorcycle_count": motorcycle_count,
     }
 
     return render(request, "home/index.html", context)
