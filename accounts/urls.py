b7ba734856053836d017e9f5ce60b2e9b311559d
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-07-03 20:18:00 +0330

    Add Account Templates
    + Profile
    + Login
    + Register

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path, include
 from . import views
 
+app_name = "accounts"
+
 urlpatterns = [
-    path("", views.account_view, name="account"),
+    path("", views.profile_view, name="profile"),
+    path("login", views.login_view, name="login"),
+    path("register", views.register_view, name="register"),
 ]
--- accounts/views.py
+++ accounts/views.py
@@ -2,5 +2,11 @@ from django.shortcuts import render
 
 # Create your views here.
 
-def account_view(request):
-    return render(request, "accounts/account.html")
+def profile_view(request):
+    return render(request, "accounts/profile.html")
+
+def login_view(request):
+    return render(request, "accounts/login.html")
+
+def register_view(request):
+    return render(request, "accounts/register.html")
