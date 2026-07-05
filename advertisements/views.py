71c0cc953ac9ed125bd186fa894690c9838fa1c2
S.Amirhosein.M <s.a.mousavi647@gmail.com>
2026-07-05 21:19:23 +0330

    Add authentication and initial context for post_ad

--- advertisements/views.py
+++ advertisements/views.py
@@ -1,7 +1,11 @@
-from django.shortcuts import render, get_object_or_404
+from django.shortcuts import render, get_object_or_404, redirect
 from django.db.models import Prefetch
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
 
 from .models import Advertisement, Image
+from vehicles.models import Brand
+from locations.models import Province
 
 
 def advertisement_detail(request, ad_id):
@@ -31,13 +35,29 @@ def advertisement_detail(request, ad_id):
         },
 )
 
+
+@login_required
 def post_ad_view(request):
-    '''
-    if not request.user.profile.reg_status:
-    messages.warning(
-        request,
-        "ابتدا اطلاعات حساب خود را تکمیل کنید."
-    )
-    return redirect("accounts:profile")
-    '''
-    return render(request, "advertisements/post_ad.html")
+
+    profile = request.user.profile
+    
+    if not profile.reg_status:
+        messages.warning(request,"ابتدا اطلاعات حساب خود را تکمیل کنید.")
+        return redirect("accounts:profile")
+    
+    context = {
+        "profile" : profile,
+
+        "brands" : Brand.objects.all(),
+
+        "provinces" : Province.objects.prefetch_related("cities").all(),
+
+        "cities": (
+            profile.default_address.city.province.cities.all()
+            if profile.default_address
+            else []
+        )
+    }
+
+    
+    return render(request, "advertisements/post_ad.html", context)
